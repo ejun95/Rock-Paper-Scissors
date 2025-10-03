475c8cab1f66fdc080aaa4ec70e3b5dda88c1498
ejun95 <55568923+ejun95@users.noreply.github.com>
2025-10-02 18:43:14 -0700

    Add function to play one round of RPS

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,20 +1,23 @@
 const MAX_NUMBER_OF_CHOICES = 3;
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-  let choice = '';
+  let choice = "";
  
   switch(getRandomNumber()) {
     case 0:
-      choice = 'rock';
+      choice = "rock";
       break;
     case 1:
-      choice = 'paper';
+      choice = "paper";
       break;
     case 2:
-      choice = 'scissors';
+      choice = "scissors";
       break;
     default:
-      choice = 'invalid';
+      choice = "invalid";
       break;
   }
   return choice;
@@ -29,3 +32,46 @@ function getHumanChoice() {
 
   return input;
 }
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  console.log("you chose " + humanChoice + ", the computer chose " + computerChoice);
+
+  if(humanChoice == "rock") {
+    if(computerChoice == "rock") {
+      console.log("Tie! No one wins this round!");
+    } else if(computerChoice == "paper") {
+      console.log("Paper covers Rock! You lose!")
+      ++computerScore;
+    } else if(computerChoice == "scissors") {
+      console.log("Rock crushes Scissors! You Win!")
+      ++humanScore;
+    }
+  } else if(humanChoice == "paper") {
+    if(computerChoice == "rock") {
+      console.log("Paper covers Rock! You Win!");
+      ++humanScore;
+    } else if(computerChoice == "paper") {
+      console.log("Tie! No one wins this round!")     
+    } else if(computerChoice == "scissors") {
+      console.log("Scissors cuts Paper! You Lose!")
+      ++computerScore;
+    }
+  } else if(humanChoice == "scissors") {
+    if(computerChoice == "rock") {
+      console.log("Rock crushes Scissors! You Lose!");
+      ++computerScore;
+    } else if(computerChoice == "paper") {
+      console.log("Scissors cuts Paper! You Win!")
+      ++humanScore;
+    } else if(computerChoice == "scissors") {
+      console.log("Tie! No one wins this round!") 
+    }
+  }
+}
+
+let humanChoice =  getHumanChoice();
+let computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
