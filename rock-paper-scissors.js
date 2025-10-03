4153b9746d35de017f6ae1c6f67b7621d9798b04
ejun95 <55568923+ejun95@users.noreply.github.com>
2025-10-02 17:36:34 -0700

    Implement getComputerChoice for RPS

---
+++ rock-paper-scissors.js
@@ -0,0 +1,25 @@
+const MAX_NUMBER_OF_CHOICES = 3;
+
+function getComputerChoice() {
+  let choice = '';
+ 
+  switch(getRandomNumber()) {
+    case 0:
+      choice = 'rock';
+      break;
+    case 1:
+      choice = 'paper';
+      break;
+    case 2:
+      choice = 'scissors';
+      break;
+    default:
+      choice = 'invalid';
+      break;
+  }
+  return choice;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * MAX_NUMBER_OF_CHOICES);
+}
